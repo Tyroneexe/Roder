402fd1b6ce5a756267e9aed159f3eafd50837bf7
Tyroneexe <tvzbothma@gmail.com>
2023-06-13 12:55:49 +0200

    Home page new layout almost done

--- lib/test.dart
+++ lib/test.dart
@@ -36,15 +36,74 @@ class _TestState extends State<Test> {
             flexibleSpace: Container(
               decoration: const BoxDecoration(
                 image: DecorationImage(
-                  image: AssetImage('assets/appicon.png'),
+                  image: AssetImage('assets/splash_screen.png'),
                   fit: BoxFit.fill,
                 ),
               ),
               alignment: Alignment.center,
-              child: const FrostedGlassBox(
-                  theWidth: 50.0, theHeight: 50.0, theChild: Text('Near Me')),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          width: 20,
+                        ),
+                        const FrostedGlassBox(
+                          theWidth: 150.0,
+                          theHeight: 80.0,
+                          theChild: Text(
+                            'All Rides',
+                            style: TextStyle(
+                                fontFamily: 'OpenSans',
+                                fontWeight: FontWeight.bold,
+                                fontSize: 22,
+                                color: Colors.black),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        const FrostedGlassBox(
+                          theWidth: 150.0,
+                          theHeight: 80.0,
+                          theChild: Text(
+                            'Near Me',
+                            style: TextStyle(
+                                fontFamily: 'OpenSans',
+                                fontWeight: FontWeight.bold,
+                                fontSize: 22,
+                                color: Colors.black),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        const FrostedGlassBox(
+                          theWidth: 150.0,
+                          theHeight: 80.0,
+                          theChild: Text(
+                            'Random',
+                            style: TextStyle(
+                                fontFamily: 'OpenSans',
+                                fontWeight: FontWeight.bold,
+                                fontSize: 22,
+                                color: Colors.black),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                ],
+              ),
             ),
-            expandedHeight: 200.0,
+            expandedHeight: 150.0,
             pinned: false,
             actions: <Widget>[
               IconButton(
@@ -58,9 +117,7 @@ class _TestState extends State<Test> {
           SliverFillRemaining(
             child: Column(
               children: [
-                SizedBox(
-                  height: 30,
-                ),
+                _titleBar(),
                 _getDBRides(),
               ],
             ),
@@ -70,140 +127,51 @@ class _TestState extends State<Test> {
     );
   }
 
-  _getDBRides() {
-    return Expanded(
-      child: FirebaseAnimatedList(
-        query: dbRef,
-        itemBuilder: (BuildContext context, DataSnapshot snapshot,
-            Animation<double> animation, int index) {
-          Map Rides = snapshot.value as Map;
-          //unique key/id of each item in db
-          Rides['key'] = snapshot.key;
-          Rides['ref'] = snapshot.child('Rides/${user.uid}');
-          print(Rides['key']);
-          return listItem(Rides: Rides);
-        },
-      ),
-    );
-  }
-
-  Widget listItem({
-    required Map Rides,
-  }) {
-    saveprefs() async {
-      SharedPreferences prefs = await SharedPreferences.getInstance();
-      await prefs.setStringList("joined_rides", joinedRides);
-    }
-
-    return Slidable(
-      startActionPane: ActionPane(
-        motion: const BehindMotion(),
-        extentRatio: 1 / 5,
-        children: [
-          SlidableAction(
-            backgroundColor: _getBGClr(Rides['Color']),
-            icon: Icons.add,
-            label: 'JOIN',
-            onPressed: (context) async {
-              print(Rides['key']);
-              if (!joinedRides.contains(Rides['key'])) {
-                joinedRides.add(Rides['key']);
-                saveprefs();
-                databaseReference
-                    .child('Rides/${Rides['key']}')
-                    .update({'Joined': Rides['Joined'] + 1});
-                _addedToFav();
-                setState(() {});
-              } else {
-                _alreadyJoined();
-              }
-            },
-          ),
-        ],
-      ),
-      child: Stack(
-        clipBehavior: Clip.none,
-        alignment: Alignment.topRight,
+  _titleBar() {
+    return Container(
+      margin: const EdgeInsets.only(right: 20, top: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Container(
-            margin: const EdgeInsets.all(10),
-            padding: const EdgeInsets.all(20),
-            height: 200,
-            decoration: BoxDecoration(
-              color: joinedRides.contains(Rides['key'])
-                  ? darkGr
-                  : _getBGClr(Rides['Color']),
-              borderRadius: BorderRadius.circular(20.0),
-            ),
-            child: Stack(
-              children: [
-                Container(
-                  // color: Colors.red.withOpacity(0.2),
-                  padding: EdgeInsets.only(left: 205, top: 100),
-                  child: Text(
-                    "Joined: ${Rides['Joined'].toString()}",
-                    style: tyStyle,
+            margin: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text(
+                  'Near Me',
+                  style: TextStyle(
+                    fontFamily: 'OpenSans',
+                    fontWeight: FontWeight.bold,
+                    fontSize: 35,
                   ),
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      margin: const EdgeInsets.only(
-                        bottom: 20,
-                      ),
-                      child: Center(
-                        child: Text(
-                          Rides['Name'],
-                          style: const TextStyle(
-                              fontFamily: 'OpenSans',
-                              fontWeight: FontWeight.bold,
-                              fontSize: 24,
-                              color: Colors.white),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 5,
-                    ),
-                    Center(
-                      child: FittedBox(
-                        fit: BoxFit.contain,
-                        child: Text(
-                          Rides['Origin'] + '  to  ' + Rides['Destination'],
-                          style: TextStyle(
-                              fontFamily: 'OpenSans',
-                              fontWeight: FontWeight.w400,
-                              fontSize: 15,
-                              color: Colors.white),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 15,
-                    ),
-                    Text(Rides['Date'], style: tyStyle),
-                    Text(
-                      Rides['Start Time'] + '  to  ' + Rides['End Time'],
-                      style: tyStyle,
-                    ),
-                  ],
                 ),
               ],
             ),
           ),
-          Positioned(
-            right: 0,
-            child: CircleAvatar(
-              radius: 25,
-              backgroundImage: NetworkImage(Rides['GPhoto']),
-            ),
-          ),
         ],
       ),
-      closeOnScroll: true,
     );
+  }
+
+  chooseTitleText(int nu) {
+    switch (nu) {
+      case 0:
+        setState(() {
+          Text('All');
+        });
+        break;
+      case 1:
+        setState(() {
+          Text('Near Me');
+        });
+        break;
+      case 2:
+        setState(() {
+          Text('Random');
+        });
+        break;
+    }
   }
 
   _getBGClr(int no) {
@@ -235,6 +203,145 @@ class _TestState extends State<Test> {
   //       return lightBlueClr;
   //   }
   // }
+
+  _getDBRides() {
+    return Expanded(
+      child: FirebaseAnimatedList(
+        physics: NeverScrollableScrollPhysics(),
+        query: dbRef,
+        itemBuilder: (BuildContext context, DataSnapshot snapshot,
+            Animation<double> animation, int index) {
+          Map Rides = snapshot.value as Map;
+          //unique key/id of each item in db
+          Rides['key'] = snapshot.key;
+          Rides['ref'] = snapshot.child('Rides/${user.uid}');
+          print(Rides['key']);
+          return listItem(Rides: Rides);
+        },
+      ),
+    );
+  }
+
+  Widget listItem({
+    required Map Rides,
+  }) {
+    saveprefs() async {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      await prefs.setStringList("joined_rides", joinedRides);
+    }
+
+    return Container(
+      child: Slidable(
+        startActionPane: ActionPane(
+          motion: const BehindMotion(),
+          extentRatio: 1 / 5,
+          children: [
+            SlidableAction(
+              backgroundColor: _getBGClr(Rides['Color']),
+              icon: Icons.add,
+              label: 'JOIN',
+              onPressed: (context) async {
+                print(Rides['key']);
+                if (!joinedRides.contains(Rides['key'])) {
+                  joinedRides.add(Rides['key']);
+                  saveprefs();
+                  databaseReference
+                      .child('Rides/${Rides['key']}')
+                      .update({'Joined': Rides['Joined'] + 1});
+                  _addedToFav();
+                  setState(() {});
+                } else {
+                  _alreadyJoined();
+                }
+              },
+            ),
+          ],
+        ),
+        child: Stack(
+          clipBehavior: Clip.none,
+          alignment: Alignment.topRight,
+          children: [
+            Container(
+              margin: const EdgeInsets.all(10),
+              padding: const EdgeInsets.all(20),
+              height: 200,
+              decoration: BoxDecoration(
+                color: joinedRides.contains(Rides['key'])
+                    ? darkGr
+                    : _getBGClr(Rides['Color']),
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              child: Stack(
+                children: [
+                  Container(
+                    // color: Colors.red.withOpacity(0.2),
+                    padding: EdgeInsets.only(left: 205, top: 100),
+                    child: Text(
+                      "Joined: ${Rides['Joined'].toString()}",
+                      style: tyStyle,
+                    ),
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.only(
+                          bottom: 20,
+                        ),
+                        child: Center(
+                          child: Text(
+                            Rides['Name'],
+                            style: const TextStyle(
+                                fontFamily: 'OpenSans',
+                                fontWeight: FontWeight.bold,
+                                fontSize: 24,
+                                color: Colors.white),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      Center(
+                        child: FittedBox(
+                          fit: BoxFit.contain,
+                          child: Text(
+                            Rides['Origin'] + '  to  ' + Rides['Destination'],
+                            style: TextStyle(
+                                fontFamily: 'OpenSans',
+                                fontWeight: FontWeight.w400,
+                                fontSize: 15,
+                                color: Colors.white),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      Text(Rides['Date'], style: tyStyle),
+                      Text(
+                        Rides['Start Time'] + '  to  ' + Rides['End Time'],
+                        style: tyStyle,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Positioned(
+              right: 0,
+              child: CircleAvatar(
+                radius: 25,
+                backgroundImage: NetworkImage(Rides['GPhoto']),
+              ),
+            ),
+          ],
+        ),
+        closeOnScroll: true,
+      ),
+    );
+  }
 
   _alreadyJoined() {
     Get.snackbar("RIDE ALREADY JOINED", "You have already joined this ride",
--- lib/ui/widgets/frosted_glass.dart
+++ lib/ui/widgets/frosted_glass.dart
@@ -16,11 +16,12 @@ class FrostedGlassBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ClipRect(
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(30),
       child: Container(
         width: theWidth,
         height: theHeight,
-        color: Colors.white,
+        color: Colors.transparent,
         child: Stack(
           children: [
             //blur effect
@@ -34,11 +35,25 @@ class FrostedGlassBox extends StatelessWidget {
             //gradient effect
             Container(
               decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(30),
-                  border: Border.all(color: Colors.white)),
-            )
+                borderRadius: BorderRadius.circular(30),
+                border: Border.all(
+                  color: Colors.white.withOpacity(0.13),
+                ),
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                    Colors.white.withOpacity(0.15),
+                    Colors.white.withOpacity(0.05)
+                  ],
+                ),
+              ),
+            ),
 
             //child
+            Center(
+              child: theChild,
+            ),
           ],
         ),
       ),
