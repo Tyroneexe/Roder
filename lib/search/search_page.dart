0238f8359bc1bc52818b7be44c5d7f89e9104086
Tyroneexe <tvzbothma@gmail.com>
2023-06-29 20:25:50 +0200

    working search page recent history

--- lib/search/search_page.dart
+++ lib/search/search_page.dart
@@ -4,7 +4,7 @@ import 'package:get/get.dart';
 import '../themes/theme.dart';
 
 class Search extends StatefulWidget {
-  const Search({super.key});
+  const Search({Key? key}) : super(key: key);
 
   @override
   State<Search> createState() => _SearchState();
@@ -12,6 +12,7 @@ class Search extends StatefulWidget {
 
 class _SearchState extends State<Search> {
   final searchFilter = TextEditingController();
+  List<String> recentHistory = [];
 
   @override
   Widget build(BuildContext context) {
@@ -47,7 +48,22 @@ class _SearchState extends State<Search> {
                 ),
               ),
             ],
-          )
+          ),
+          SizedBox(height: 10),
+          Expanded(
+            child: ListView.builder(
+              itemCount: recentHistory.length,
+              itemBuilder: (context, index) {
+                return Row(
+                  children: [
+                    Text(
+                      recentHistory[index],
+                    ),
+                  ],
+                );
+              },
+            ),
+          ),
         ],
       ),
     );
@@ -59,10 +75,14 @@ class _SearchState extends State<Search> {
       height: 45,
       decoration: BoxDecoration(
         color: searchBarClr,
-        borderRadius: BorderRadius.circular(6),
+        borderRadius: BorderRadius.circular(
+          6,
+        ),
       ),
       child: Padding(
-        padding: const EdgeInsets.only(left: 20.0),
+        padding: const EdgeInsets.only(
+          left: 20.0,
+        ),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
@@ -109,8 +129,12 @@ class _SearchState extends State<Search> {
                 ),
               ),
               onPressed: () {
-                searchFilter.clear(); // Clear the text
-                setState(() {});
+                final searchText = searchFilter.text;
+                if (searchText.isNotEmpty) {
+                  recentHistory.insert(0, searchText);
+                  searchFilter.clear();
+                  setState(() {});
+                }
               },
             ),
           ],
@@ -119,24 +143,3 @@ class _SearchState extends State<Search> {
     );
   }
 }
-
-        // Container(
-        //     height: 45,
-        //     width: MediaQuery.of(context).size.width - 40,
-        //     decoration: BoxDecoration(
-        //       color: searchBarClr,
-        //       borderRadius: BorderRadius.circular(6),
-        //     ),
-        //     child: Padding(
-        //       padding: const EdgeInsets.all(8.0),
-        //       child: Text(
-        //         'Search',
-        //         style: TextStyle(
-        //           fontFamily: 'Roboto',
-        //           fontWeight: FontWeight.w700,
-        //           fontSize: 18,
-        //           color: searchBarTxtClr,
-        //         ),
-        //       ),
-        //     ),
-        //   ),    
