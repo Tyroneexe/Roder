2be0d33d2ff9cb841630297e4eb9a38c5a7601ff
Tyroneexe <tvzbothma@gmail.com>
2023-06-30 20:21:54 +0200

    added a bike form

--- lib/account/account_page.dart
+++ lib/account/account_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unrelated_type_equality_checks
+
 import 'package:flutter/material.dart';
 import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
@@ -18,6 +20,7 @@ class _AccountPageState extends State<AccountPage> {
   TextEditingController nuController = TextEditingController();
   TextEditingController emailController = TextEditingController();
   TextEditingController locationController = TextEditingController();
+  TextEditingController bikeController = TextEditingController();
 
   @override
   void initState() {
@@ -29,176 +32,256 @@ class _AccountPageState extends State<AccountPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _appBar(),
-      body: Column(
-        children: [
-          Center(
-            child: Stack(
-              alignment: Alignment.bottomRight,
-              children: [
-                CircleAvatar(
-                  radius: 60,
-                  backgroundImage: NetworkImage(user.photoURL!),
-                ),
-                Container(
-                  width: 36,
-                  height: 36,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: searchBarClr,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Center(
+              child: Stack(
+                alignment: Alignment.bottomRight,
+                children: [
+                  CircleAvatar(
+                    radius: 60,
+                    backgroundImage: NetworkImage(user.photoURL!),
                   ),
-                  child: Icon(
-                    Icons.edit,
-                    color: btnBlueClr,
-                    size: 24,
-                  ),
-                ),
-                Positioned(
-                  right: 0,
-                  bottom: 0,
-                  child: Container(
+                  Container(
                     width: 36,
                     height: 36,
                     decoration: BoxDecoration(
                       shape: BoxShape.circle,
-                      border: Border.all(
-                        color: Colors.white,
-                        width: 2,
+                      color: searchBarClr,
+                    ),
+                    child: Icon(
+                      Icons.edit,
+                      color: btnBlueClr,
+                      size: 24,
+                    ),
+                  ),
+                  Positioned(
+                    right: 0,
+                    bottom: 0,
+                    child: Container(
+                      width: 36,
+                      height: 36,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          color: Colors.white,
+                          width: 2,
+                        ),
                       ),
                     ),
                   ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Text(
+                      'User Name',
+                      style: actPageTxt,
+                    ),
+                  ],
                 ),
+                SizedBox(
+                  height: 10,
+                ),
+                userNameForm(context),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Text(
+                      'Contact Number',
+                      style: actPageTxt,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                numberForm(context),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Text(
+                      'Email',
+                      style: actPageTxt,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                emailForm(context),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Text(
+                      'Location',
+                      style: actPageTxt,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                locationForm(context),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Text(
+                      'Bike',
+                      style: actPageTxt,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                bikeForm(context),
+                // Spacer()
               ],
             ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Container bikeForm(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width - 40,
+      height: 40,
+      child: TextFormField(
+        style: TextStyle(
+            fontFamily: 'Roboto',
+            fontWeight: FontWeight.w100,
+            fontSize: 14,
+            color: Colors.black),
+        controller: bikeController,
+        decoration: InputDecoration(
+          hintText: 'Rather Not Say',
+          hintStyle: TextStyle(
+            fontFamily: 'Roboto',
+            fontWeight: FontWeight.w100,
+            fontSize: 14,
+            color: Colors.black,
           ),
-          SizedBox(
-            height: 20,
+          contentPadding: EdgeInsets.only(
+            left: 20,
           ),
-          Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    width: 20,
-                  ),
-                  Text(
-                    'User Name',
-                    style: actPageTxt,
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              userNameForm(context),
-              SizedBox(
-                height: 20,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    width: 20,
-                  ),
-                  Text(
-                    'Contact Number',
-                    style: actPageTxt,
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              numberForm(context),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    width: 20,
-                  ),
-                  Text(
-                    'Email',
-                    style: actPageTxt,
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              emailForm(context),
-              SizedBox(
-                height: 20,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    width: 20,
-                  ),
-                  Text(
-                    'Location',
-                    style: actPageTxt,
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                width: MediaQuery.of(context).size.width - 40,
-                height: 40,
-                child: TextFormField(
-                  readOnly: true,
-                  style: TextStyle(
-                      fontFamily: 'Roboto',
-                      fontWeight: FontWeight.w100,
-                      fontSize: 14,
-                      color: Colors.black),
-                  controller: emailController,
-                  decoration: InputDecoration(
-                    hintText: '$_currentCity, $_currentCountry',
-                    hintStyle: TextStyle(
-                      fontFamily: 'Roboto',
-                      fontWeight: FontWeight.w100,
-                      fontSize: 14,
-                      color: Colors.black,
-                    ),
-                    contentPadding: EdgeInsets.only(
-                      left: 20,
-                    ),
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: btnBlueClr,
-                        width: 1.5,
-                      ),
-                      borderRadius: BorderRadius.circular(
-                        6,
-                      ),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: btnBlueClr,
-                        width: 1.5,
-                      ),
-                      borderRadius: BorderRadius.circular(
-                        6,
-                      ),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: btnBlueClr,
-                        width: 1.5,
-                      ),
-                      borderRadius: BorderRadius.circular(
-                        6,
-                      ),
-                    ),
-                  ),
-                ),
-              )
-            ],
+          border: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: btnBlueClr,
+              width: 1.5,
+            ),
+            borderRadius: BorderRadius.circular(
+              6,
+            ),
           ),
-        ],
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: btnBlueClr,
+              width: 1.5,
+            ),
+            borderRadius: BorderRadius.circular(
+              6,
+            ),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: btnBlueClr,
+              width: 1.5,
+            ),
+            borderRadius: BorderRadius.circular(
+              6,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Container locationForm(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width - 40,
+      height: 40,
+      child: TextFormField(
+        readOnly: true,
+        style: TextStyle(
+            fontFamily: 'Roboto',
+            fontWeight: FontWeight.w100,
+            fontSize: 14,
+            color: Colors.black),
+        controller: locationController,
+        decoration: InputDecoration(
+          hintText: '$_currentCity, $_currentCountry',
+          hintStyle: TextStyle(
+            fontFamily: 'Roboto',
+            fontWeight: FontWeight.w100,
+            fontSize: 14,
+            color: Colors.black,
+          ),
+          contentPadding: EdgeInsets.only(
+            left: 20,
+          ),
+          border: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: btnBlueClr,
+              width: 1.5,
+            ),
+            borderRadius: BorderRadius.circular(
+              6,
+            ),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: btnBlueClr,
+              width: 1.5,
+            ),
+            borderRadius: BorderRadius.circular(
+              6,
+            ),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: btnBlueClr,
+              width: 1.5,
+            ),
+            borderRadius: BorderRadius.circular(
+              6,
+            ),
+          ),
+        ),
       ),
     );
   }
