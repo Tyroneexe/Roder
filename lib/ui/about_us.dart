00c0cbdb385d3941aa73ec7b5f088fa55d9f0533
Tyroneexe <tvzbothma@gmail.com>
2023-07-01 22:37:12 +0200

    done with about us page

--- lib/ui/about_us.dart
+++ lib/ui/about_us.dart
@@ -45,7 +45,7 @@ class _AboutUsPageState extends State<AboutUsPage> {
                       right: 20,
                     ),
                     child: Container(
-                      height: 320,
+                      height: 450,
                       width: MediaQuery.of(context).size.width,
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(
@@ -54,7 +54,68 @@ class _AboutUsPageState extends State<AboutUsPage> {
                         color: newNotis,
                       ),
                       child: Column(
-                        children: [],
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(
+                              top: 20,
+                              left: 20,
+                            ),
+                            child: Text(
+                              'Your Ride. Your Way',
+                              style: TextStyle(
+                                fontFamily: 'Roboto',
+                                fontWeight: FontWeight.w700,
+                                fontSize: 16,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(
+                              left: 20,
+                              right: 20,
+                            ),
+                            child: Text(
+                              'Our app was created with a single goal in mind: to provide for riders to discover new routes and enjoy delicious breakfasts at the same time!\n\nOur team of dedicated developers and motorcycle enthusiasts have created an app that is easy to use. With Roder, you can:',
+                              style: TextStyle(
+                                fontFamily: 'Roboto',
+                                fontWeight: FontWeight.w100,
+                                fontSize: 14,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(
+                              left: 20,
+                            ),
+                            child: Text(
+                              'How We Work',
+                              style: TextStyle(
+                                fontFamily: 'Roboto',
+                                fontWeight: FontWeight.w700,
+                                fontSize: 16,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          firstBullet(),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          secondBullet(),
+                        ],
                       ),
                     ),
                   ),
@@ -64,6 +125,68 @@ class _AboutUsPageState extends State<AboutUsPage> {
           ),
         ],
       ),
+    );
+  }
+
+  Row secondBullet() {
+    return Row(
+      children: [
+        SizedBox(
+          width: 20,
+        ),
+        Padding(
+          padding: const EdgeInsets.only(
+            bottom: 55,
+          ),
+          child: Text('●'),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(
+            left: 20,
+            right: 20,
+          ),
+          child: Text(
+            "Connect with other riders: Our app\nallows you to connect with other\nbikers who share your passion for\nbreakfast runs.",
+            style: TextStyle(
+              fontFamily: 'Roboto',
+              fontWeight: FontWeight.w100,
+              fontSize: 14,
+              color: Colors.black,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Row firstBullet() {
+    return Row(
+      children: [
+        SizedBox(
+          width: 20,
+        ),
+        Padding(
+          padding: const EdgeInsets.only(
+            bottom: 35,
+          ),
+          child: Text('●'),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(
+            left: 20,
+            right: 20,
+          ),
+          child: Text(
+            "Plan your ride: Once you've found a\ngreat breakfast spot. Use our app to\nplan Your ride.",
+            style: TextStyle(
+              fontFamily: 'Roboto',
+              fontWeight: FontWeight.w100,
+              fontSize: 14,
+              color: Colors.black,
+            ),
+          ),
+        ),
+      ],
     );
   }
 
